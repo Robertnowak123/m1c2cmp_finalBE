6420f95b11566df56bc203394162fc7299a74743
Robert Nowak <robert.andreas.nowak@gmail.com>
2016-01-31 20:52:41 +0100

    added some jquery regarding arrays and finding values within arrays for dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
+	//
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
@@ -48,38 +48,119 @@ $(document).on('page:change', function(event){
     });
 
 
+
+
+	// $("select").bind("change", function() {
+
+	// 	// name = $(this).attr("name");
+	// 	value_geography = $('#geography').val();
+	// 	value_commission = $('#commission').val();
+	// 	value_hyperlocal = $('#hyperlocal').val();
+	// 	value_category = $('#category').val();
+	// 	value_name = $('#name').val();
+	// 	value_delivery = $('#delivery').val();
+	// 	// selector = "[data-" + name + " = '" + value + "']";
+
+	// 	selector_geography = "[data-geography = '" + value_geography + "']";
+	// 	selector_commission = "[data-commission = '" + value_commission + "']";
+	// 	selector_hyperlocal = "[data-name = '" + value_hyperlocal + "']";
+	// 	selector_category = "[data-name = '" + value_category + "']";
+	// 	selector_name = "[data-name = '" + value_name + "']";
+	// 	selector_delivery = "[data-name = '" + value_delivery + "']";
+
+ //    	var filters = $.map($(selector_geography, selector_commission, selector_hyperlocal, selector_category, selector_name, selector_delivery).toArray(), function(e){
+ //        return $(e).val();
+ //    	}).join(".");
+
+	// 	$("div.large_rowBox").hide();
+
+	// 	($(selector_geography).closest("div.large_rowBox")) && ($(selector_commission).closest("div.large_rowBox")).each(function(){
+	// 			$(this).show();
+	// 	}); 
+
+
+	// 	console.log(selector_name);
+	// 	console.log(selector_geography);
+	// 	console.log(selector_commission);
+	// 	console.log(selector_category);
+	// 	console.log(selector_hyperlocal);
+	// 	console.log(selector_delivery);
+ //    });
+
+
+
+
+	// $("select").on("change", function() {
+	// 	name = $(this).attr("name");
+	// 	value = $(this).val();
+	// 	selector = "[data-" + name + "='" + value + "']";
+
+	// 	$("div.large_rowBox").hide();
+	// 	$(selector).closest("div.large_rowBox").each(function(){
+
+	// 		$(this).show();
+	// 	});
+	// 	console.log(selector);
+ //    });
+
+
+	var filters = [];
 	$("select").bind("change", function() {
+
 		name = $(this).attr("name");
 		value = $(this).val();
 		selector = "[data-" + name + "='" + value + "']";
+
+		
+		// filters.push($(this).val());
+		filters.push(value);
+		var filtersum = filters.join();
+
+
+		
+
 		$("div.large_rowBox").hide();
-		$(selector).closest("div.large_rowBox").each(function(){
+		$(selector).closest("div.large_rowBox:contains('" + filtersum + "')").each(function(){
+	
+			$(this).show();	
+		}); 
 
-			$(this).show();
-		});
 		console.log(selector);
-//		$("[data-geography='true']").closest("div.large_rowBox").toggle();
-//     	$(".database_table_image:not(." + this.value + ")").each(function(){
-//     	$(this).toggle();
+		// console.log(filters);
+		console.log(filtersum);
     });
 
-	$("#tag_hyperlocal").bind("change", function() {
-		$("[data-hyperlocal='true']").closest("div.large_rowBox").toggle();
-//     	$(".database_table_image:not(." + this.value + ")").each(function(){
-//     	$(this).toggle();
-    });
 
-// 	$("#tag_hyperlocal").bind("change", function() {
-// 		$("[data-hyperlocal='true']").closest("div.large_rowBox").toggle();
-// //     	$(".database_table_image:not(." + this.value + ")").each(function(){
-// //     	$(this).toggle();
-//     });    
 
-// 	$("#tag_hyperlocal").bind("change", function() {
-// 		$("[data-hyperlocal='true']").closest("div.large_rowBox").toggle();
-// //     	$(".database_table_image:not(." + this.value + ")").each(function(){
-// //     	$(this).toggle();
-//     });
+
+
+
+
+
+
+
+
+
+
+
+	// $("select").on("change", function() {
+	// 	name = $(this).attr("name");
+	// 	value = $(this).val();
+	// 	selector = "[data-" + name + "='" + value + "']";
+
+	// 	$("div.large_rowBox").hide();
+	// 	$(selector).closest("div.large_rowBox").each(function(){
+
+	// 		$(this).show();
+	// 	});
+	// 	console.log(selector);
+ //    });
+
+	// $("#tag_hyperlocal").bind("change", function() {
+	// 	$("[data-hyperlocal='true']").closest("div.large_rowBox").toggle();
+ //    });
+
+
 
 
 
